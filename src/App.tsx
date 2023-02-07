ab9c7cff3a989f8b7e5a120a7f52ec4e399d7331
joko19 <jokoslamet2207@gmail.com>
2023-02-07 23:29:55 +0700

    update data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import { instance } from "./instance";
+
+interface AppProps {
+  title: string;
+  author:string
+}
+
 function App() {
-  return <h1 className="text-3xl font-bold text-center mt-12">Hello world!</h1>;
+  const [data, setData] = useState<AppProps[]>([]);
+
+  useEffect(() => {
+    fetch(`${instance}/api/posts`)
+      .then((res) => res.json())
+      .then((result) => setData(result));
+  }, []);
+  
+  return (
+    <h1 className="text-3xl font-bold text-center mt-12">
+      {data.map((item, index) => (
+        <div key={index}>
+          <div>{item.title}</div>
+          <div>{item.author}</div>
+        </div>
+      ))}
+    </h1>
+  );
 }
 
 export default App;
---
+++ src/instance.js
@@ -0,0 +1,2 @@
+// export const instance = "http://localhost:3001"
+export const instance = "https://college-json-server.vercel.app/"
